ba092d42cd86e0d9e36007215cef04c82c3d2c8d
Ytsixx <evaristobilly52@gmail.com>
2025-02-19 08:10:47 +0200

    pagina layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
@@ -49,6 +51,9 @@ export default function Home() {
           </a>
         </div>
       </main>
+      
+      
+      
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -80,9 +85,11 @@ export default function Home() {
           />
           Examples
         </a>
+        
+        
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="/layout"
           target="_blank"
           rel="noopener noreferrer"
         >
