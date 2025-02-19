b545c781446594b2ebe52beab9ecdacde29e1c51
Ytsixx <evaristobilly52@gmail.com>
2025-02-19 08:39:13 +0200

    pagina layou2t

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+/*import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export default function Home() {
@@ -28,4 +28,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-}
+}*/
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+export default function Home() {
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,4 +27,5 @@ export default function RootLayout({ children }) {
       </body>
     </html>
   );
+}
 }
