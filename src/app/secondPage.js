4340a002a69dbebeae33d1edb5ad48ae38b20fb4
Ytsixx <evaristobilly52@gmail.com>
2025-02-21 13:40:50 +0200

    paginag

---
+++ src/app/secondPage.js
@@ -0,0 +1,8 @@
+// pages/secondPage.js
+export default function SecondPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1>Bem-vindo à segunda página!</h1>
+    </div>
+  );
+}
