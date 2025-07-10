781137ec320048f967edde9639b2350e33695e8e
Ytsixx <evaristobilly52@gmail.com>
2025-07-10 11:57:34 +0200

    Mensagem clara e objetiva explicando a mudança

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <h1>Bem-vindo à Página Inicial</h1>
+      <h1>eu sou sixx</h1>
       <Link href="/agora">
         <button>Ir para outra página</button>
       </Link>
